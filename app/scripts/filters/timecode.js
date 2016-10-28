998fb09ca6846ec800f363eba1bb354e7ef5089e
FEFelson <edwrdwalsh@gmail.com>
2016-10-27 20:08:12 -0400

    Adds timecode filter

---
+++ app/scripts/filters/timecode.js
@@ -0,0 +1,32 @@
+(function() {
+    function timecode() {
+        return function(seconds) {
+            
+            var output = "--:--";
+            var  seconds = Number.parseFloat(seconds);
+            
+            
+            if (! Number.isNaN(seconds))
+            {
+                var wholeSeconds = Math.floor(seconds);
+                var minutes = Math.floor(wholeSeconds / 60);
+                var remainingSeconds = wholeSeconds % 60;
+
+                output = minutes + ":";
+
+                if (remainingSeconds < 10) 
+                {
+                    output += "0";
+                }
+
+                output += remainingSeconds;
+            }
+            
+            return output;
+        };
+    }
+    
+    angular
+        .module("blocJams")
+        .filter("timecode", timecode)
+})();
